9e9ca60a9ea8d30b135e486146a48985e45ce1db
Roman Kuk <34649365+RomanCook@users.noreply.github.com>
2018-09-04 23:36:21 +0300

    Add files via upload

---
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -0,0 +1,19 @@
+const rootNode = document.getElementById("root");
+
+
+button.onclick = function () {
+
+    let add = document.getElementById("text").value();
+
+
+}
+
+
+function newRow() {
+    let div = document.createElement('div')
+    div.setAttribute("id", "mark")
+    let div2 = document.createElement('div')
+    div2.setAttribute("id", "input")
+    let div3 = document.createElement('div')
+    div3.setAttribute("id", "trash")
+}
