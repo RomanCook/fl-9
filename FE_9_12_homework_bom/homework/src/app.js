fee21464b8d53db8fac66c0c5ef202aad3394836
Roman Kuk <34649365+RomanCook@users.noreply.github.com>
2018-09-09 23:12:18 +0300

    Add files via upload

---
+++ FE_9_12_homework_bom/homework/src/app.js
@@ -0,0 +1,9 @@
+const rootNode = document.getElementById('root');
+
+const todoItems = [
+    {isDone: false, id: 12345, description: 'Todo 1'}
+];
+
+// See you next time...
+
+rootNode.appendChild(/* Append your list item node*/);
